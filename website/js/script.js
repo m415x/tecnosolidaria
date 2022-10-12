7ce8d3db90ace81eb94240213f94cf89f0ca54fe
Cristian Lahoz <lahozcristian@gmail.com>
2022-10-12 17:54:44 -0300

    Validación de Formulario

---
+++ website/js/script.js
@@ -0,0 +1,59 @@
+
+/*--------------------------*/
+/** FORMULARIO DE CONTACTO **/
+/*--------------------------*/
+
+/* Mostrar input Asunto: "otro" */
+function mostrar(id) {
+  if (id == "otro") {
+    document.getElementById("otro").style.display = ""
+    document.form.other.focus()
+  } else {
+    document.getElementById("otro").style.display = "none"
+  }
+}
+
+/* Validar Formulario */
+function validarFormulario() {
+  // Nombre
+  if (document.form.firstname.value.length == 0) {
+    alert("Debes completar tu nombre")
+    document.form.firstname.focus()
+    return 0
+  }
+  // Apellido
+  if (document.form.lastname.value.length == 0) {
+    alert("Debes completar tu apellido")
+    document.form.lastname.focus()
+    return 0
+  }
+  // Email
+  let emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
+  if (document.form.email.value.length == 0) {
+    alert("Debes agregar una dirección de email")
+    document.form.email.focus()
+    return 0
+  } else if (emailRegex.test(document.form.email.value) != true) {
+    alert("Debes agregar una dirección de email válida")
+    document.form.email.focus()
+    return 0
+  }
+  // Teléfono
+  let telRegex = /^(?:(?:00)?549?)?0?(?:11|[2368]\d)(?:(?=\d{0,2}15)\d{2})??\d{8}$/g
+  if (document.form.phonenumber.value.length > 0) {
+    if (telRegex.test(document.form.phonenumber.value) != true) {
+      alert("Debes agregar un número de teléfono válido")
+      document.form.phonenumber.focus()
+      return 0
+    }
+  }
+  // Asunto
+  if (document.form.lastname.value.length == 0) {
+    alert("Debes completar tu apellido")
+    document.form.lastname.focus()
+    return 0
+  }
+
+  alert("Muchas gracias")
+  document.form.submit()
+}
