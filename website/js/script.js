06e6476ee5a59853d7258dc9c15b11962f03c665
Cristian Lahoz <lahozcristian@gmail.com>
2022-10-16 01:00:02 -0300

    Cabecera pegajosa y menu hamburguesa

--- website/js/script.js
+++ website/js/script.js
@@ -15,26 +15,6 @@ window.onscroll = function () {
 }
 
 
-/*---------------------*/
-/** MENÚ HAMBURGUESA **/
-/*--------------------*/
-
-// const toggleButton = document.getElementById('button-menu')
-// const navWrapper = document.getElementById('nav')
-
-// toggleButton.addEventListener('click',() => {
-//   toggleButton.classList.toggle('close')
-//   navWrapper.classList.toggle('show')
-// })
-
-// navWrapper.addEventListener('click',e => {
-//   if(e.target.id === 'nav'){
-//     navWrapper.classList.remove('show')
-//     toggleButton.classList.remove('close')
-//   }
-// })
-
-
 /*--------------------------*/
 /** FORMULARIO DE CONTACTO **/
 /*--------------------------*/
